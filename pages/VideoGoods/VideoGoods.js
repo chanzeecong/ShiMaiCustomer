778060cd1f1a0e3dddd04fe2da5c1fdbcc4f86b9
Elmer🐣 <13632657943@163.com>
2019-03-09 19:44:37 +0800

    paypage update

--- pages/VideoGoods/VideoGoods.js
+++ pages/VideoGoods/VideoGoods.js
@@ -22,12 +22,15 @@ Page({
     is_followed: false,
     isCollected: false,
     isLike: false,
+    showModalStatus: false,
+    amount: 1, //初始数量
+    gg_id: ``,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     let id = options.id;
     let type = options.type;
     let buyer_id = options.buyer_id
@@ -42,7 +45,7 @@ Page({
 
     let self = this;
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         self.setData({
           scrollHeight: res.windowHeight
         })
@@ -229,7 +232,7 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
@@ -237,7 +240,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.initPageData();
     this.saveFootList();
   },
@@ -248,39 +251,235 @@ Page({
     this.setData({
       detailList: detailList,
     });
+
+    this.setCountDown();
+  },
+
+
+  setCountDown() {
+    for (let i in this.data.detailList) {
+      let endDate = this.data.detailList[i].end_time;
+
+      if (!endDate) {
+        continue;
+      }
+      let arr = endDate.split(` `);
+      let arr0 = arr[0].split(`-`);
+      let arr1 = arr[1].split(`:`);
+
+      this.countDown(Number(arr0[0]), Number(arr0[1]) - 1, Number(arr0[2]), Number(arr1[0]), Number(arr1[1]), Number(arr1[2]), Number(i))
+    }
+  },
+
+  countDown(yy, MM, dd, hh, mm, ss, index) {
+    let date = new Date(yy, MM, dd, hh, mm, ss);
+
+    let deadlineTime = date.getTime();
+    let nowDate = new Date();
+    let nowTime = nowDate.getTime();
+    let distTime = deadlineTime - nowTime;
+
+    if (distTime > 0) {
+      let day = Math.floor(distTime / 1000 / 3600 / 24);
+      let hour = Math.floor(distTime / 1000 / 3600 % 24);
+      let minute = Math.floor(distTime / 1000 / 60 % 60);
+      let second = Math.floor(distTime / 1000 % 60);
+
+      day = (day <= 9) ? `0${day}` : day;
+      hour = (hour <= 9) ? `0${hour}` : hour;
+      minute = (minute <= 9) ? `0${minute}` : minute;
+      second = (second <= 9) ? `0${second}` : second;
+
+      this.data.detailList[index].countDownDay = day;
+      this.data.detailList[index].countDownHour = hour;
+      this.data.detailList[index].countDownMin = minute;
+      this.data.detailList[index].countDownSec = second;
+
+      this.setData({
+        detailList: this.data.detailList
+      })
+
+      setTimeout(() => {
+        this.countDown(yy, MM, dd, hh, mm, ss, index);
+      }, 1000);
+    } else {
+      this.data.detailList[index].countDownDay = 0;
+      this.data.detailList[index].countDownHour = 0;
+      this.data.detailList[index].countDownMin = 0;
+      this.data.detailList[index].countDownSec = 0;
+    }
+  },
+
+  buyGoods() {
+    this.showModal();
+  },
+
+  showModal() {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+
+  hideModal: function() {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
+  },
+
+  filter: function(e) {
+    let id = e.currentTarget.dataset.id;
+    let index = e.currentTarget.dataset.index;
+    let gg_id = e.currentTarget.dataset.gg;
+
+    for (let i in this.data.detailList) {
+
+      if (id === this.data.detailList[i].id) {
+
+        for (let j in this.data.detailList[i].good_attributes) {
+
+          this.data.detailList[i].good_attributes[j].isChoose = false;
+        }
+        this.data.detailList[i].good_attributes[index].isChoose = true;
+      }
+    }
+
+    this.setData({
+      detailList: this.data.detailList,
+      gg_id: gg_id
+    })
+  },
+
+  confirm_buy(e) {
+    let good_attr_id = this.data.gg_id;
+    console.log(good_attr_id)
+    let index = e.currentTarget.dataset.index;
+    let home = this.data.detailList[index];
+    let sizeList = home.good_attributes;
+    let isChoose = false;
+    let attrId = 0;
+
+    for (let size of sizeList) {
+      if (size.isChoose) {
+        isChoose = true;
+        attrId = size.id;
+        break;
+      }
+    }
+    if (isChoose) {
+      wx.navigateTo({
+        url: ``,
+      })
+    } else {
+      wx.showToast({
+        title: '你不选买个锤子',
+        icon: `none`
+      })
+    }
+
+    return new Promise(resolve => {
+      wx.request({
+        url: `${app.hostName}order`,
+        method: 'POST',
+        header: {
+          'Authorization': `Bearer ${app.token}`
+        },
+        data: {
+          id: this.data.good_attr_id,
+          amount: this.data.amount.toString(),
+        },
+        dataType: 'json',
+        success: (res) => {
+          resolve(res.data.data);
+        }
+      })
+    })
+
+  },
+
+
+  /* 点击减号 */
+  bindMinus: function() {
+    let detailList = this.data.detailList;
+
+    let amount = Math.max(this.data.amount - 1, 1);
+
+    this.setData({
+      detailList: detailList,
+      amount: amount
+    })
+  },
+  /* 点击加号 */
+  bindPlus: function() {
+    let detailList = this.data.detailList;
+    let amount = Math.min(this.data.amount + 1, 9)
+
+    this.setData({
+      detailList: detailList,
+      amount: amount
+    })
   },
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
